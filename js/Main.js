c056f69b83fa25cca88a7e6612d61a83b818bccb
PigFat2000 <Archi.Zadrot@mail.ru>
2015-12-26 14:24:22 +0500

    vot

--- js/Main.js
+++ js/Main.js
@@ -1,9 +1,12 @@
  function init() {
-    var stage = new createjs.Stage("demoCanvas");
+ 	canvas = document.getElementById('mnomCanvas');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+    var stage = new createjs.Stage("mnomCanvas");
     var rect = new createjs.Shape();
-	rect.graphics.beginFill("DeepSkyBlue").drawRect(0, 0, 50, 50);
-	rect.x = 100;
-	rect.y = 100;
+	rect.graphics.beginFill("DeepSkyBlue").drawRect(-50, -50, 100, 100);
+	rect.x = canvas.width/2;
+	rect.y = canvas.height/2;
 	stage.addChild(rect);
 	stage.update();
   }
